ebd29e2d75e8d2267fc183b369bd7f8bd0d36c0e
Flavia Barreto <flavia.t.barreto@gmail.com>
2012-11-12 18:44:50 -0300

    SlideShow

--- views/index/index.php
+++ views/index/index.php
@@ -1,105 +1,197 @@
-<div id='transparencia'>	
-	<a target="_blank" href="http://www.ba.portaldatransparencia.com.br/prefeitura/joaodourado/doe/"><div id="transparencia_municipal"></div></a>
-</div>
-<div id=busca>
-	<input type="text" id="busca_texto" value="Buscar..." onfocus="this.value=''"/>
-	<p><a href="">Ok</a></p>
-</div>
-<div id='news'>
-   <div id='titulo_news'>
-      <p>Notícias</p>
-   </div>
+  <div id='transparencia'>	
+  	<a target="_blank" href="http://www.ba.portaldatransparencia.com.br/prefeitura/joaodourado/doe/"><div id="transparencia_municipal"></div></a>
+  </div>
+  <div id=busca>
+  	<input type="text" id="busca_texto" value="Buscar..." onfocus="this.value=''"/>
+  	<p><a href="">Ok</a></p>
+  </div>
+  <div id='news'>
+     <div id='titulo_news'>
+        <p>Notícias</p>
+     </div>
 
-   <div id="slider">
-      <ul id="sliderContent">
-         <li class="sliderImage">
-            <a href=""><img src="<?php echo URL?>/css/images/news/1.jpg" alt="1" /></a>
-            <span class="top"><strong>Noticia 1</strong><br />Essa seria a chamada para noticia 1</span>
-         </li>
-         <li class="sliderImage">
-            <a href=""><img src="<?php echo URL?>/css/images/news/2.jpg" alt="2" /></a>
-            <span class="top"><strong>Noticia 2</strong><br />Essa seria a chamada para noticia 2</span>
-         </li>
-         <li class="sliderImage">
-            <img src="<?php echo URL?>/css/images/news/3.jpg" alt="3" />
-            <span class="top"><strong>Noticia 3</strong><br />Essa seria a chamada para noticia 3</span>
-         </li>
-         <div class="clear sliderImage"></div>
-      </ul>
-   </div>
-
-   <div id='news_list'>
-      <h3> Mais Notícias</h3>
-      <p>
-         <div class='news_date'>11/07/2012</div>
-         <div class='news_title'>Noticia 1</div>
-      </p>
-      <hr color='#E5FF80'/>
-      <p>
-         <div class='news_date'>11/07/2012</div>
-         <div class='news_title'>Noticia 2</div>
-      </p>
-      <hr color='#E5FF80'/>
-      <p>
-         <div class='news_date'>11/07/2012</div>
-         <div class='news_title'>Noticia 3</div>
-      </p>
-      <hr color='#E5FF80'/>
-      <p>
-         <div class='news_date'>11/07/2012</div>
-         <div class='news_title'>Noticia 4</div>
-      </p>
-      <hr color='#E5FF80'/>
-      <p>
-         <div class='news_date'>11/07/2012</div>
-         <div class='news_title'>Noticia 5</div>
-      </p>
-   </div>
-
-</div>
-
-<div id="plugins">
-   <div class="fb-like-box" data-href="http://www.facebook.com/platform"
-		data-width="185" data-height="350" data-show-faces="true" data-stream="false" data-header="false">
-	</div>
-</div>
-
-<div id='acontece'>
-   <div id='titulo_acontece'>
-      <p>PMJD Acontece</p>
-   </div>
+  <div id="jslidernews2" class="lof-slidecontent" style="width:740px; height:300px;">
+  	<div class="preload"><div></div></div>
+              
+      <div  class="button-previous">Previous</div>
+        <!-- MAIN CONTENT --> 
+        <div class="main-slider-content" style="width:740px; height:300px;">
+                  <ul class="sliders-wrap-inner">
+                      <li>
+                              <img src="<?php echo URL?>/css/images/slide/thumbl_980x340.png" title="Newsflash 2" >           
+                            
+                      </li> 
+                     <li>
+                        <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_002.png" title="Newsflash 1" >           
+                           
+                      </li> 
+                     <li>
+                        <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_003.png" title="Newsflash 3" >            
+                          
+                      </li> 
+                    </ul>  	
+              </div>
+   		   <!-- END MAIN CONTENT --> 
+             <!-- NAVIGATOR -->
+             	<div class="navigator-content">
+                    <div class="navigator-wrapper">
+                          <ul class="navigator-wrap-inner">
+                            <li>
+                                  <div>
+                                      <img src="<?php echo URL?>/css/images/slide/791902news3.jpg" />
+                                      <h3> Noticia 1 </h3>
+                                      <span>20.01.2010</span> - Um Subtitulo
+                                  </div>    
+                              </li>
+                               <li>
+                                  <div>
+                                      <img src="<?php echo URL?>/css/images/slide/435576news10.jpg" />
+                                      <h3> Noticia 2 </h3>
+                                      <span>20.01.2010</span> - Um Subtitulo
+                                  </div>    
+                              </li>
+                  
+                              <li>
+                                  <div>
+                                      <img src="<?php echo URL?>/css/images/slide/641906img1.jpg" />
+                                      <h3> Noticia 3 </h3>
+                                      <span>20.01.2010</span> - Um Subtitulo
+                                  </div>     
+                              </li>
+                              
+                             <li>
+                                  <div>
+                                      <img src="<?php echo URL?>/css/images/slide/416719news7.jpg" />
+                                      <h3> NewsFlash 4</h3>
+                                      <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                  </div>
+                              </li>    
+                              <li>
+                                   <div>
+                                      <img src="<?php echo URL?>/css/images/slide/641906img1.jpg" />
+                                      <h3> NewsFlash 5</h3>
+                                      <span>20.01.2010</span> -In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                   </div>   
+                              </li>
+                             <li>
+                                  <div>
+                                      <img src="<?php echo URL?>/css/images/slide/416719news7.jpg" />
+                                      <h3> NewsFlash 6</h3>
+                                      <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                  </div>
+                              </li>     		
+                          </ul>
+                    </div>
+     
+               </div> 
+            <!----------------- END OF NAVIGATOR --------------------->
+            <div class="button-next">Next</div>
    
-   <div id="acontece_video">
-      <iframe width="430" height="315" src="http://www.youtube.com/embed/0qDEfvWetZY" 
-              frameborder="0" allowfullscreen></iframe>
-   </div>
-   
-   <div id="acontece_botoes">
-      <a href="http://www.youtube.com/user/tvpmjoaodourado/" target="_blank" class="acontece_botao">
-         
-         <div id="acontece_botao_video"></div>   
-      </a>
-      <a href="#" target="_blank" class="acontece_botao">
-         <div id="acontece_botao_foto"></div>   
-      </a>
-      <a href="#" target="_blank">
-         <div id="acontece_botao_radio"></div>
-         
-      </a>
-      <div id="midias_sociais">
+   		 <!-- BUTTON PLAY-STOP -->
+            <div class="button-control"><span></span></div>
+            <!-- END OF BUTTON PLAY-STOP -->
+             
+   </div> 
+
+
+
+
+  </div>
+
+  <div id="plugins">
+     <div class="fb-like-box" data-href="http://www.facebook.com/platform"
+  		data-width="185" data-height="350" data-show-faces="true" data-stream="false" data-header="false">
+  	</div>
+  </div>
+
+  <div id='acontece'>
+     <div id='titulo_acontece'>
+        <p>PMJD Acontece</p>
+     </div>
+     
+     <div id="acontece_video">
+        <iframe width="430" height="315" src="http://www.youtube.com/embed/0qDEfvWetZY" 
+                frameborder="0" allowfullscreen></iframe>
+     </div>
+     <div id="imagens_acontece">
+  	   <div id="page-margins">
+  	    <div class="example">
+          <div class="ppy" id="ppy3">
+              <ul class="ppy-imglist">
+                  <li><a href="http://farm4.static.flickr.com/3142/3053892214_390c0365df.jpg"><img src="http://farm4.static.flickr.com/3142/3053892214_390c0365df_m.jpg" alt="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cras euismod risus non diam." /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3014/3053902638_7871e32230.jpg"><img src="http://farm4.static.flickr.com/3014/3053902638_7871e32230_m.jpg" alt="In tempus consequat purus. Curabitur in massa nec purus semper adipiscing. Donec convallis odio nec nibh. Nam viverra, tortor nec cursus accumsan, purus purus ultricies mauris, in dignissim orci dui at est. Morbi non quam at ante cursus posuere. Sed ornare sem vitae diam tempor blandit. Nam risus diam, faucibus eget, fermentum et, dignissim eu, tellus. Praesent non justo. Proin vestibulum diam at mi. Cras molestie eleifend mi. Morbi massa enim, auctor at, rhoncus nec, consequat nec, diam. In laoreet lacus vel velit. Sed vulputate commodo ligula." /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3136/3053075339_ca3f303a6d.jpg"><img src="http://farm4.static.flickr.com/3136/3053075339_ca3f303a6d_m.jpg" alt="Sed consequat tincidunt sapien." /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3234/3053054359_090927b527.jpg"><img src="http://farm4.static.flickr.com/3234/3053054359_090927b527_m.jpg" alt="Proin aliquam libero ac lectus. Sed consequat tincidunt sapien. Suspendisse hendrerit porta ante. Proin aliquam libero ac lectus. Sed consequat tincidunt sapien. Suspendisse hendrerit porta ante." /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3161/3053878060_16a1ed4786.jpg"><img src="http://farm4.static.flickr.com/3161/3053878060_16a1ed4786_m.jpg" alt="Aenean tristique pellentesque quam. Proin aliquam libero ac lectus." /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3247/3053897646_df5b74c794.jpg"><img src="http://farm4.static.flickr.com/3247/3053897646_df5b74c794_m.jpg" alt="" /></a></li>
+                  <li><a href="http://farm4.static.flickr.com/3225/3053039525_2d3ef0c58e.jpg"><img src="http://farm4.static.flickr.com/3225/3053039525_2d3ef0c58e_m.jpg" alt="Suspendisse hendrerit porta ante." /></a></li>
+              </ul>
+              <div class="ppy-outer">
+                  <div class="ppy-stage">
+                      <div class="ppy-nav">
+                          <div class="nav-wrap">
+                              <a class="ppy-prev" title="Previous image">Previous image</a>
+                              <a class="ppy-play" title="Play Slideshow">Play Slideshow</a>
+                              <a class="ppy-pause" title="Stop Slideshow">Stop Slideshow</a>
+                              <a class="ppy-next" title="Next image">Next image</a>
+                          </div>
+                      </div>
+                      <div class="ppy-counter">
+                          <strong class="ppy-current"></strong> / <strong class="ppy-total"></strong> 
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-   </div>
-</div>
+      </div>
+  	</div>
+     <div id="sublinha_acontece"></div>
+     <div id="acontece_sub">
+  		<div id="midias_sociais"></div>
+  		<div id="acontece_videos"></div>
+  		<div id="acontece_audios"></div>
+  		<div id="acontece_fotos"></div>
+     </div>
+  </div>
 
-<div id='obras'>
-   <div id='titulo_obras'>
-      <p>A Prefeitura trabalhando - Obras</p>
-   </div>
-   <a href=""><img src="<?php echo URL?>/css/images/obra.png" /></a>
-</div>
+  <div id='obras'>
+     <div id='titulo_obras'>
+        <p>A Prefeitura trabalhando - Obras</p>
+     </div>
+     <!--<a href=""><img src="<?php echo URL?>/css/images/obra.png" /></a> !-->
+  		<div id="slider">
+  		  <ul id="sliderContent">
+  			 <li class="sliderImage">
+  				<a href=""><img src="<?php echo URL?>/css/images/1.jpg" alt="1" /></a>
+  				<span class="top"><strong>Noticia 1</strong><br />Essa seria a chamada para noticia 1</span>
+  			 </li>
+  			 <li class="sliderImage">
+  				<a href=""><img src="<?php echo URL?>/css/images/2.jpg" alt="2" /></a>
+  				<span class="top"><strong>Noticia 2</strong><br />Essa seria a chamada para noticia 2</span>
+  			 </li>
+  			 <li class="sliderImage">
+  				<img src="<?php echo URL?>/css/images/news/3.jpg" alt="3" />
+  				<span class="top"><strong>Noticia 3</strong><br />Essa seria a chamada para noticia 3</span>
+  			 </li>
+  			 <div class="clear sliderImage"></div>
+  		  </ul>
+  	   </div>
+  </div>
 
-<div id='quadrados'>
-		<img src='<?php echo URL?>/css/images/home_newsletter.png' class="quadrado_img" id="quadrado_newsletter"/>	
-   <img src='<?php echo URL?>/css/images/home_producao.png' class="quadrado_img" id="quadrado_producao"/>
-   <img src='<?php echo URL?>/css/images/home_ouvidoria.png' class="quadrado_img" id="quadrado_ouvidoria"/>
-</div>
+  <div id='quadrados'>
+  		<img src='<?php echo URL?>/css/images/home_newsletter.png' class="quadrado_img" id="quadrado_newsletter"/>	
+     <img src='<?php echo URL?>/css/images/home_producao.png' class="quadrado_img" id="quadrado_producao"/>
+     <img src='<?php echo URL?>/css/images/home_ouvidoria.png' class="quadrado_img" id="quadrado_ouvidoria"/>
+  </div>
+
+  <!--
+
+    <div class="slider-description">
+                            <div class="slider-meta"><a target="_parent" title="Newsflash 4" href="#Category-4">/ Newsflash 4 /</a> <i> — Monday, February 15, 2010 12:42</i></div>
+                              <h4>Content of Newsflash 4</h4>
+                              <p>Joomla! 1.5 - 'Experience the Freedom'!. It has never been easier to create your own dynamic Web...
+                              <a class="readmore" href="#">Read more </a>
+                              </p>
+                           </div>
+
+                           -->
