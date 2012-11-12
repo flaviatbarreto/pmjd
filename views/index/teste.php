0b08a19850e7e1336848346ea3287e98ea3e949d
Flavia Barreto <flavia.t.barreto@gmail.com>
2012-11-12 18:57:31 -0300

    SlideShow_News

---
+++ views/index/teste.php
@@ -0,0 +1,133 @@
+<div id="jslidernews2" class="lof-slidecontent" style="width:740px; height:300px;">
+	<div class="preload"><div></div></div>
+            
+            <div  class="button-previous">Previous</div>
+                   
+    		 <!-- MAIN CONTENT --> 
+              <div class="main-slider-content" style="width:684px; height:300px;">
+                <ul class="sliders-wrap-inner">
+                    <li>
+                            <img src="<?php echo URL?>/css/images/slide/thumbl_980x340.png" title="Newsflash 2" >           
+                          <div class="slider-description">
+                            <div class="slider-meta"><a target="_parent" title="Newsflash 1" href="#Category-1">/ Newsflash 1 /</a> <i> — Monday, February 15, 2010 12:42</i></div>
+                            <h4>Content of Newsflash 1</h4>
+                            <p>The one thing about a Web site, it always changes! Joomla! makes it easy to add Articles, content,...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                   <li>
+                      <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_002.png" title="Newsflash 1" >           
+                         <div class="slider-description">
+                           <div class="slider-meta"><a target="_parent" title="Newsflash 2" href="#Category-2">/ Newsflash 2 /</a> 	<i> — Monday, February 15, 2010 12:42</i></div>
+                            <h4>Content of Newsflash 2</h4>
+                            <p>Joomla! makes it easy to launch a Web site of any kind. Whether you want a brochure site or you are...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                   <li>
+                      <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_003.png" title="Newsflash 3" >            
+                        <div class="slider-description">
+                          <div class="slider-meta"><a target="_parent" title="Newsflash 3" href="#Category-3">/ Newsflash 3 /</a> 	<i> — Monday, February 15, 2010 12:42</i></div>
+                            <h4>Content of Newsflash 3</h4>
+                            <p>With a library of thousands of free Extensions, you can add what you need as your site grows. Don't...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                    <li>
+                      <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_004.png" title="Newsflash 5" >            
+                        <div class="slider-description">
+                          <div class="slider-meta"><a target="_parent" title="Newsflash 4" href="#Category-4">/ Newsflash 4 /</a>	<i> — Monday, February 15, 2010 12:42</i></div>
+                            <h4>Content of Newsflash 4</h4>
+                            <p>Joomla! 1.5 - 'Experience the Freedom'!. It has never been easier to create your own dynamic Web...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                    <li>
+                      <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_005.png" title="Newsflash 5" >            
+                        <div class="slider-description">
+                           <div class="slider-meta"><a target="_parent" title="Newsflash 5" href="#">/ Newsflash 5 /</a>	<i> — Monday, February 15, 2010 12:42</i></div>
+                           <h4>Content of Newsflash 5</h4>
+                            <p>Joomla! 1.5 - 'Experience the Freedom'!. It has never been easier to create your own dynamic Web...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                    <li>
+            
+                      <img src="<?php echo URL?>/css/images/slide/thumbl_980x340_006.png" title="Newsflash 5" >            
+                        <div class="slider-description">
+                          <div class="slider-meta"><a target="_parent" title="Newsflash 6" href="#">/ Newsflash 6 /</a>	<i> — Monday, February 15, 2010 12:42</i></div>
+                            <h4>Content of Newsflash 6</h4>
+                            <p>Joomla! 1.5 - 'Experience the Freedom'!. It has never been easier to create your own dynamic Web...
+                            <a class="readmore" href="#">Read more </a>
+                            </p>
+                         </div>
+                    </li> 
+                
+                  </ul>  	
+            </div>
+ 		   <!-- END MAIN CONTENT --> 
+           <!-- NAVIGATOR -->
+           	<div class="navigator-content">
+                  <div class="navigator-wrapper">
+                        <ul class="navigator-wrap-inner">
+                          <li>
+                                <div>
+                                    <img src="<?php echo URL?>/css/images/slide/791902news3.jpg" />
+                                    <h3> NewsFlash 1 </h3>
+                                    <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu...
+                                </div>    
+                            </li>
+                             <li>
+                                <div>
+                                    <img src="<?php echo URL?>/css/images/slide/435576news10.jpg" />
+                                    <h3> NewsFlash 2 </h3>
+                                    <span>20.01.2010</span> -In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                </div>    
+                            </li>
+                
+                            <li>
+                                <div>
+                                    <img src="<?php echo URL?>/css/images/slide/641906img1.jpg" />
+                                    <h3> NewsFlash 3 </h3>
+                                    <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                </div>     
+                            </li>
+                            
+                           <li>
+                                <div>
+                                    <img src="<?php echo URL?>/css/images/slide/416719news7.jpg" />
+                                    <h3> NewsFlash 4</h3>
+                                    <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                </div>
+                            </li>    
+                            <li>
+                                 <div>
+                                    <img src="<?php echo URL?>/css/images/slide/641906img1.jpg" />
+                                    <h3> NewsFlash 5</h3>
+                                    <span>20.01.2010</span> -In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                 </div>   
+                            </li>
+                           <li>
+                                <div>
+                                    <img src="<?php echo URL?>/css/images/slide/416719news7.jpg" />
+                                    <h3> NewsFlash 6</h3>
+                                    <span>20.01.2010</span> - In id, mauris viverra asperiores, bibendum in id. Eu molestie. Ac sit eu. ..
+                                </div>
+                            </li>     		
+                        </ul>
+                  </div>
+   
+             </div> 
+          <!----------------- END OF NAVIGATOR --------------------->
+          <div class="button-next">Next</div>
+ 
+ 		 <!-- BUTTON PLAY-STOP -->
+          <div class="button-control"><span></span></div>
+          <!-- END OF BUTTON PLAY-STOP -->
+           
+ </div> 
